ca89c7b3aab26859462bbd2677a43eef5294dff1
TheSherlockHomie <coolman.kushagra@gmail.com>
2015-09-04 20:23:11 +0530

    create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*#include"stdafx.h"
+#include"targetver.h"
+#include"Vector.h"
+#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	cVector v0, v1(2.0, 2.0,0), v2(2.0, 2.0, 1);
+	v0 = v1 + v2;
+	double a1 = v0.giveX(), a2 = v0.giveY(), a3 = v0.giveMagnitude(), a4 = v0.giveAngle();
+	cout << a1 << endl << a2 << endl << a3 << endl<<a4<<endl;
+	system("pause");
+	
+}//
+*/
